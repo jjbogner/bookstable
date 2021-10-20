811296bd92597d781b18a6b737989fee356cc30f
jjbogner <89399865+jjbogner@users.noreply.github.com>
2021-10-20 19:23:36 -0400

    Add files via upload

---
+++ app/data/schema.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE IF NOT EXISTS msisdb;
+USE msisdb;
+
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+	id int PRIMARY KEY AUTO_INCREMENT ,
+    username varchar(24) UNIQUE NOT NULL,
+    name varchar(48)
+);
+
+INSERT INTO student (id, name) VALUES 
+(1, 'Tom Gregory'),
+(2, 'Beth Barnhart'),
+(3, 'Prof. Prabhakar');
+
+-- SELECT * FROM students;
+
+DROP TABLE IF EXISTS offer;
+CREATE TABLE offer (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    studentId int NOT NULL REFERENCES student(id) 
+        ON DELETE CASCADE ON UPDATE CASCADE,
+	title VARCHAR(50) NOT NULL DEFAULT '',
+    author VARCHAR(50) NOT NULL DEFAULT '',
+    year_pub VARCHAR(4) NOT NULL DEFAULT '',
+    publisher VARCHAR(50) NOT NULL DEFAULT '',
+    pg_count int(4) NOT NULL DEFAULT 0,
+    MSRP int NOT NULL DEFAULT 0
+);
+);
+
+-- Student 1 has no offers, Student 2 has 3 offers, Student 3 has one offer
+INSERT INTO offer(id, studentId, companyName, salary, bonus, offerDate) VALUES
+  (1, 2, 'KPMG', 95000, 7000, '2021-09-30'),
+  (2, 2, 'Deloitte Digital', 94000, 12000, '2021-10-03'),
+  (3, 2, 'IU, ISGP', 54000, 0, '2021-10-05'),
+  (4, 3, 'Amazon', 122000, 11000, '2021-10-15')
+;
+
+-- COMMIT;
+
+-- CREATE USER 'msisreader'@'%' IDENTIFIED BY 'msisreadonly';
+-- GRANT SELECT ON * . * TO 'msisreader'@'%';
+
